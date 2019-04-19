29b41013e736379556c5b5a132a1bd4a000d0d99
Unknown <unknown@example.com>
2019-04-19 16:28:04 +0200

    feat: Added signature

---
+++ src/lib/signature.js
@@ -0,0 +1,24 @@
+utils = require('./utils')
+md5 = require('js-md5')
+
+function createSignature(obj, method, params, timestamp, secret) {
+    const orderedParams = utils.orderObject(params)
+
+    let paramsString = ''
+    utils.iterateObject(orderedParams, (key, value) => paramsString += key + value)
+
+    let signatureString =
+        obj +
+        method +
+        paramsString +
+        timestamp +
+        secret
+
+    signature = md5(signatureString)
+
+    return signature;
+}
+
+module.exports = {
+    createSignature: createSignature
+}
---
+++ src/test/signature.test.js
@@ -0,0 +1,17 @@
+const signature = require('../lib/signature')
+
+test('Generated signature is correct', () => {
+    expectedSignature = 'e6d57b97a24be62be88e725e51a23a38'
+
+    expect(signature.createSignature(
+        'track',
+        'getFileUrl',
+        {
+            'format_id': '6',
+            'intent': 'stream',
+            'track_id': '59754717'
+        },
+        '1555671409',
+        '421125438'
+    )).toBe(expectedSignature)
+})

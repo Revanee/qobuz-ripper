cb8730946ac13ffa43b80417cf57a489af3ea428
Hristo Kamenov <hkamenov123@gmai.com>
2019-04-19 19:07:03 +0200

    feat: Added generateTrackRequestOptions

--- src/lib/request.js
+++ src/lib/request.js
@@ -1,5 +1,6 @@
 const request = require('request')
 const signature = require('./signature')
+const utils = require('./utils')
 
 function generateQueryParameters(parameters) {
     return parameters
@@ -17,7 +18,33 @@ function generateUrl(location, query) {
     return location + '?' + query
 }
 
+function generateTrackRequestOptions(trackId, timestamp, conf) {
+    const method = 'getFileUrl'
+    const object = 'track'
+    const parameters = {
+        track_id: trackId,
+        format_id: '6',
+        intent: 'stream'
+    }
+    const sig = signature.createSignature(object, method, parameters, timestamp, conf.secret)
+    console.log('Signature: ' + sig)
+    parameters.request_ts = timestamp
+    parameters.request_sig = sig
+    const allParameters = utils.objectToArray(parameters)
+    const query = generateQueryParameters(allParameters)
+    const url = generateUrl(conf.trackURL, query)
+    return {
+        url: url,
+        method: "GET",
+        headers: {
+            'X-App-Id': conf.appId,
+            'X-User-Auth-Token': conf.token
+        }
+    }
+}
+
 module.exports = {
     generateQueryParameters: generateQueryParameters,
-    generateUrl: generateUrl
+    generateUrl: generateUrl,
+    generateTrackRequestOptions: generateTrackRequestOptions
 }
